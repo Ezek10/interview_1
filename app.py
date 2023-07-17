106087ccd1a29a560014df3ebfd829d28ca1b7c3
Eze Marcel <ezequielmarcel2@gmail.com>
2023-07-17 12:59:12 -0300

    create fastapi app

---
+++ app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from fight import Fight
+from logger import logger
+from players.arnaldor_shuatseneguer import ArnaldorShuatseneguer
+from players.tonyn_stallone import TonynStallone
+from request import FightRequest
+
+app = FastAPI()
+
+
+@app.post("/fight")
+async def talana_jrpg(request: FightRequest) -> list[str]:
+    player1 = request.player1.to_player(TonynStallone)
+    player2 = request.player2.to_player(ArnaldorShuatseneguer)
+    fight = Fight(player1, player2)
+    fight.start()
+    content = logger.get_all_logs()
+    logger.clean()
+    return JSONResponse(status_code=200, content=content)
