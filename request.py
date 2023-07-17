1f055c57a52d63fd4350229e3c8d6d58ffd35bac
Eze Marcel <ezequielmarcel2@gmail.com>
2023-07-17 12:58:29 -0300

    create request model

---
+++ request.py
@@ -0,0 +1,24 @@
+import pydantic as pd
+
+from players.model import Player
+
+
+class Movimiento(pd.BaseModel):
+    mov: str = pd.Field(pattern="^[AWSDawsd]{0,5}$")
+
+
+class Golpe(pd.BaseModel):
+    golpe: str = pd.Field(pattern="^[PKpk]{0,1}$")
+
+
+class PlayerInput(pd.BaseModel):
+    movimientos: list[str]
+    golpes: list[str]
+
+    def to_player(self, player: Player) -> Player:
+        return player(self.movimientos, self.golpes)
+
+
+class FightRequest(pd.BaseModel):
+    player1: PlayerInput
+    player2: PlayerInput
