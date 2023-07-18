a015a7ff2fc4514e4becf41bca19c9e02cb714fe
Eze Marcel <ezequielmarcel2@gmail.com>
2023-07-17 22:24:47 -0300

    fix: input str constrains

--- src/request.py
+++ src/request.py
@@ -3,22 +3,14 @@ import pydantic as pd
 from src.players.model import Player
 
 
-class Movimiento(pd.BaseModel):
-    mov: str = pd.Field(pattern="^[AWSDawsd]{0,5}$")
-
-
-class Golpe(pd.BaseModel):
-    golpe: str = pd.Field(pattern="^[PKpk]{0,1}$")
-
-
 class PlayerInput(pd.BaseModel):
-    movimientos: list[str]
-    golpes: list[str]
+    movements: list[pd.constr(pattern="^[AWSDawsd]{0,5}$")] = pd.Field(alias = "movimientos")
+    attacks: list[pd.constr(pattern="^[PKpk]{0,1}$")] = pd.Field(alias = "golpes")
 
     def to_player(self, player: Player) -> Player:
-        return player(self.movimientos, self.golpes)
+        return player(self.movements, self.attacks)
 
 
 class FightRequest(pd.BaseModel):
     player1: PlayerInput
-    player2: PlayerInput
+    player2: PlayerInput = None
